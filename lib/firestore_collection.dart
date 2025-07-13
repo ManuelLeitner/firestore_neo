bfbdd5355d60066ccec71f7d30c1cdd8f8878847
ManuelLeitner <manuel@leitner.systems>
2025-07-13 23:52:13 +0200

    provide unfiltered stream

--- lib/firestore_collection.dart
+++ lib/firestore_collection.dart
@@ -16,12 +16,8 @@ abstract class FirestoreCollectionBase<T extends JsonObject> {
   /// However document id cannot be filtered
   bool loadAll;
 
-  FirestoreCollectionBase(
-      {required this.fromJson,
-      required this.firestoreNeo,
-      required this.loadAll}) {
-    assert(
-        T != JsonObject, "pass real type (not JsonObject) as generic argument");
+  FirestoreCollectionBase({required this.fromJson, required this.firestoreNeo, required this.loadAll}) {
+    assert(T != JsonObject, "pass real type (not JsonObject) as generic argument");
   }
 
   bool isApplicable(WrapColRef ref);
@@ -31,12 +27,8 @@ abstract class FirestoreCollectionBase<T extends JsonObject> {
   }
 }
 
-class FirestoreDeserializer<T extends JsonObject>
-    extends FirestoreCollectionBase<T> {
-  FirestoreDeserializer(
-      {required super.fromJson,
-      required super.firestoreNeo,
-      required super.loadAll});
+class FirestoreDeserializer<T extends JsonObject> extends FirestoreCollectionBase<T> {
+  FirestoreDeserializer({required super.fromJson, required super.firestoreNeo, required super.loadAll});
 
   @override
   bool isApplicable(WrapColRef ref) {
@@ -44,32 +36,34 @@ class FirestoreDeserializer<T extends JsonObject>
   }
 }
 
-abstract class FirestoreQuery<T extends JsonObject>
-    extends FirestoreCollectionBase<T> {
+abstract class FirestoreQuery<T extends JsonObject> extends FirestoreCollectionBase<T> {
   Query<Document> query;
 
-  Stream<List<T>> get stream async* {
+  Stream<List<T>> get filteredStream async* {
     debugPrint("stream $query");
     await for (var snap in query.snapshots()) {
-      var raw =
-          await DependencyLoader.loadObjectList<T>(firestoreNeo, snap.docs);
+      var raw = await DependencyLoader.loadObjectList<T>(firestoreNeo, snap.docs);
       raw.sort();
       yield raw;
     }
   }
 
-  FirestoreQuery(
-      {required this.query,
-      required super.fromJson,
-      required super.firestoreNeo,
-      required super.loadAll});
+  FirestoreQuery({required this.query, required super.fromJson, required super.firestoreNeo, required super.loadAll});
 }
 
 class FirestoreCollection<T extends JsonObject> extends FirestoreQuery<T> {
   WrapColRef path;
 
-  FirestoreCollection(FirestoreNeo firestoreNeo, this.path,
-      T Function(Document d) fromJson, bool loadAll,
+  Stream<List<T>> get unfilteredStream async* {
+    debugPrint("stream $query");
+    await for (var snap in path.ref.snapshots()) {
+      var raw = await DependencyLoader.loadObjectList<T>(firestoreNeo, snap.docs);
+      raw.sort();
+      yield raw;
+    }
+  }
+
+  FirestoreCollection(FirestoreNeo firestoreNeo, this.path, T Function(Document d) fromJson, bool loadAll,
       {Query<Document> Function(Query<Document> query)? configureQuery})
       : super(
             firestoreNeo: firestoreNeo,
@@ -127,8 +121,7 @@ class FirestoreCollection<T extends JsonObject> extends FirestoreQuery<T> {
   }
 
   Future<List<T>> getList() async {
-    var res = await DependencyLoader.loadObjectList<T>(
-        firestoreNeo, await query.getDocs());
+    var res = await DependencyLoader.loadObjectList<T>(firestoreNeo, await query.getDocs());
 
     if (res.firstOrNull is Comparable<T>) {
       res.sort();
